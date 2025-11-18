4646742c7d0424475b99a45c9304166d5e5cf834
Ulrich Aïvodji <a.u.matchi@gmail.com>
2025-11-18 13:52:12 -0500

    add accepted papers

--- utils.py
+++ utils.py
@@ -10,3 +10,31 @@ def yml_reviewers():
     with open(yml_path, "w") as f:
         yaml.safe_dump(items, f, sort_keys=False, allow_unicode=True)
 
+def get_website_data(sheet_name=""):
+    sheet_id = "11ngfJidLjbk_DA-4PUXkb7MPbIMVfAcGDWphDtB4Ppk"  # Replace with your actual Sheet ID
+    # sheet_name = "HQPs"  # Optional if targeting a specific sheet
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+    df = pd.read_csv(url)
+
+    return df
+
+
+def dump_data():
+    dump_papers()
+
+
+def dump_papers():
+    df = get_website_data("papers")
+
+    df_dict = df.to_dict(orient="records")
+
+
+    df_oral = df[df["oral"] == True]
+    df_oral_dict = df_oral.to_dict(orient="records")
+
+    with open("./data/papers.yml", "w") as file:
+        yaml.dump(df_dict, file, default_flow_style=False)
+
+    with open("./data/oral-presentations.yml", "w") as file:
+        yaml.dump(df_oral_dict, file, default_flow_style=False)
